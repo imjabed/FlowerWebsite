b4645f6dcb9ab43abd836f2e5aac4fa757f2e568
imjabed <contactabujabed@gmail.com>
2025-07-21 13:33:06 +0530

    cors allowed for everyone

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const app = express()
 require('dotenv').config()
 
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+  origin: '*', //domain need
+  credentials: true
+}));
 
 
 app.use('/api/auth', require('./routes/authRoute'))
