28a5d00c63ca8196b6be62f4495582f3d51a2a65
imjabed <contactabujabed@gmail.com>
2025-07-21 14:30:08 +0530

    cors fixedd

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,15 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); 
+app.options('/*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://ourflowerwebsite.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
+
+
 
 
 app.use(express.json())
